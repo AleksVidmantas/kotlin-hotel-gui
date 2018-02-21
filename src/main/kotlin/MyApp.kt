7ed95380844a38826e4ce3c747f49faeda138392
Alex <bobopirate1010@gmail.com>
2018-02-20 23:07:29 -0800

    Work on input field.

--- src/main/kotlin/MyApp.kt
+++ src/main/kotlin/MyApp.kt
@@ -1,40 +1,34 @@
-
 import javafx.application.Application
-import javafx.scene.control.Button
-import javafx.scene.control.Label
-import javafx.scene.layout.BorderPane
-import javafx.scene.layout.VBox
-import tornadofx.App
-
-
-import tornadofx.View
+import tornadofx.*
 
 class MyApp: App(MyView::class)
 
-class MyView: View() {
+class MyView : View() {
 
-    val topView: TopView by inject()
-    val bottomView: BottomView by inject()
+    val controller: MyController by inject()
 
-    override val root = BorderPane()
+    override val root = vbox {
 
-    init {
-        with(root){
-            top = topView.root
-            bottom = bottomView.root
+        label("Input")
+
+        val inputField = textfield()
+
+        button("Commit") {
+            action {
+                controller.writeToDb(inputField.text)
+                inputField.clear()
+            }
         }
-
     }
+}
 
+
+class MyController: Controller() {
+    fun writeToDb(inputValue: String) {
+        println("Writing $inputValue to database!")
+    }
 }
 
 fun main(args: Array<String>) {
     Application.launch(MyApp::class.java, *args)
-}
-
-class TopView: View(){
-    override val root = Label("Top View")
-}
-class BottomView: View(){
-    override val root = Label("Bottom View")
 }
